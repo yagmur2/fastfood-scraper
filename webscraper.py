c0e272daea72295b4e5ad2d9b1c7fb5b5e9d76e3
Yagmur Eff <43115705+yagmur2@users.noreply.github.com>
2019-12-16 21:36:18 -0500

    Created function htmlToSoup Input: website url, Output: BeautifulSoup representation of contents

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
 import requests
 from requests import Timeout
 import json
@@ -8,22 +8,31 @@ import re
 basePage = 'https://fastfoodnutrition.org'
 hubPage = 'https://fastfoodnutrition.org/fast-food-restaurants'
 
-# Attempts a connection to the website. If the exception is thrown, the website is down (nothing we can do).
-try:
-    response = requests.get(
-        hubPage, headers={'User-Agent': 'Mozilla/5.0'}, timeout=(2, 5))
-except Timeout:
-    print('Request timed out. Check server connection.')
+# Sends a request to the website, then converts the HTML response to BeautifulSoup structure.
+# If the exception is thrown, the website is down (nothing we can do).
 
-# BeautifulSoup object representing the HTML contents
-content = soup(response.content, "html.parser")
+
+def htmlToSoup(website):
+    try:
+        response = requests.get(
+            website, headers={'User-Agent': 'Mozilla/5.0'}, timeout=(2, 5))
+        # print(response) #--used for debugging
+    except Timeout:
+        print('Request timed out. Check server connection.')
+    soup = BeautifulSoup(response.content, "html.parser")
+    return soup
+
+
+hub = htmlToSoup(hubPage)
 data = []
 
 # Finds all <a> tags in the card view
-for i in content.findAll('a', attrs={"class": "divider"}):
+for i in hub.findAll('a', attrs={"class": "divider"}):
+    href = i.get('href')
+
     restaurantObject = {
-        "link": hubPage + i.get('href'),
-        "name": i.get('href')[1:].capitalize()
+        "link": hubPage + href,
+        "name": href[1:].capitalize()
     }
     data.append(restaurantObject)
 
@@ -32,8 +41,3 @@ with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
 
 #images = content.findAll('div', attrs={"class": "logo_box_image lozad"})
-
-print(response)  # HTTP Status code for debugging purposes
-#print(names)
-#print(images)
-#print (restaurants)
