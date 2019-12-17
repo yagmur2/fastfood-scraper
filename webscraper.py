4baa34d4a1f91e95ad6076b9ea38ba78fedec197
Yagmur Eff <43115705+yagmur2@users.noreply.github.com>
2019-12-16 22:42:43 -0500

    Lots of additions/changes: Variables switched from 'fooPage' to 'foo' for clarity. Added logic for pulling data from individual restaurant menus. Added "menu" list and "logo" string values to JSON. General formatting/commenting improvements.

--- webscraper.py
+++ webscraper.py
@@ -5,13 +5,12 @@ import json
 import re
 
 # Don't change this! This is the website we are pulling the restaurant names from.
-basePage = 'https://fastfoodnutrition.org'
-hubPage = 'https://fastfoodnutrition.org/fast-food-restaurants'
+base = 'https://fastfoodnutrition.org'
+hub = 'https://fastfoodnutrition.org/fast-food-restaurants'
+data = [] # JSON data dump list
 
 # Sends a request to the website, then converts the HTML response to BeautifulSoup structure.
 # If the exception is thrown, the website is down (nothing we can do).
-
-
 def htmlToSoup(website):
     try:
         response = requests.get(
@@ -22,22 +21,30 @@ def htmlToSoup(website):
     soup = BeautifulSoup(response.content, "html.parser")
     return soup
 
+hubPage = htmlToSoup(hub)
 
-hub = htmlToSoup(hubPage)
-data = []
-
-# Finds all <a> tags in the card view
-for i in hub.findAll('a', attrs={"class": "divider"}):
+# Loops through each restaurant page, assigning a fresh set of variables for each.
+for i in hubPage.findAll('a', attrs={"class": "divider"}):
     href = i.get('href')
+    menuPage = htmlToSoup(base + href)
+    logoURL = base + menuPage.find('img', attrs={"class": "logo_float"})['src']
+    name = menuPage.find('h3').text
+    menu = []
+    cals = []
+
+    # Fills menu list
+    for k in menuPage.findAll('u', attrs={"class": "rest_item_list"}):
+        menu.append(k.get('title'))
+        foodItem = htmlToSoup()
 
     restaurantObject = {
-        "link": hubPage + href,
-        "name": href[1:].capitalize()
+        "link": base + href,
+        "logo": logoURL,
+        "name": name,
+        "menu": menu
     }
     data.append(restaurantObject)
 
 # Dumps the collected data into data.json
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
-
-#images = content.findAll('div', attrs={"class": "logo_box_image lozad"})
